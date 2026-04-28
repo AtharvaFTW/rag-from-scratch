e955ff123895de9a78ad08c4f11a6f39d46a9794
AtharvaFTW <atharvavankhede@gmail.com>
2026-04-28 17:33:58 +0530

    feat: added chunker

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -13,9 +13,9 @@ def load_document(pdf_path: Path) -> str:
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
             extracted = page.extract_text()
-            content = content + extracted if extracted else ""
+            content = content + extracted if extracted else content
 
-    return content if content else ""
+    return content
 
 def clean_text(text: str) -> str:
     """
@@ -30,7 +30,7 @@ def clean_text(text: str) -> str:
 
     return text
 
-def chunk_text(text: str, chunk_size: int, overlap: int) -> list[dict]:
+def chunker(text: str, chunk_size: int, overlap_size: int) -> list[dict]:
     """
     This functions breaks down the corpus into chunks with given size. 
 
@@ -39,13 +39,54 @@ def chunk_text(text: str, chunk_size: int, overlap: int) -> list[dict]:
         chunk_size: size of each chunk
         overlap: size of text overlap between two chunks
     """
-    pass
+    
+    bracket = chunk_size * 4 # Assuming the 1 token ~ 4 characters
+    overlap = overlap_size * 4 # Assuming the 1 token ~ 4 characters
+
+    # Sliding window
+
+    l = 0 
+    r = 0
+    chunk_index = 0
+
+    res = []
+
+    while (l + bracket) <= len(text):
+
+        r = l + bracket
+        chunk_text = text[l:r]
+
+        chunk = {
+            "text":str(chunk_text),
+            "chunk_index": chunk_index,
+            "source": "unknown"}
+        
+        res.append(chunk)
+        
+        l = r - overlap
+        chunk_index +=1
+
+    # Fetch whatever can't fit the window    
+    if (r - overlap) < len(text): 
+        chunk = {
+            "text": str(text[(r - overlap): len(text)]),
+            "chunk_index": chunk_index,
+            "source": "unknown"
+        }
+
+        res.append(chunk)
+    
+    return res
+
 
 
 if __name__ == "__main__":
     data = load_document(r"data\raw\the_prevention_of_cruelty_to_animals_act_1960.pdf")
-    print("before")
-    print(data[:500])
     clean = clean_text(data)
-    print("after")
-    print(clean[:500])
+    chunks = chunker(clean, chunk_size = 512, overlap_size = 50)
+    print(f"Total chunks: {len(chunks)}")
+    print(chunks[0])
+    print(chunks[1])
+    assert chunks[1]["text"][:50*4] == chunks[0]["text"][-50*4:]
+    
+    
